042211919d98b4d76c20ee978c4bf89f7c1a8d6d
chalexan <cherednychenko.alexandr@gmail.com>
2021-04-21 16:37:50 +0300

    add sessions

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,11 +1,32 @@
 // import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import Nav from '../Nav/Nav'
 import Registration from '../Registration/Registration';
 import Login from "../Login/Login";
+import Test from "../Test/Test";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+
 
 function App() {
-  return (
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    tryAuth();
+  },[]);
+
+   async function tryAuth () {
+   const preResult = await fetch('http://localhost:8080/users/auth');
+   if (preResult.status === 401) return
+   const result = await preResult.json();
+   const login = result.login;
+   const id = result.id;
+   if (login) dispatch({ type: 'LOGIN', payload: login });
+  }
+
+  const { isLogin, userName } = useSelector((state) => state);
+     return (
     <div>
      <BrowserRouter>
        <Nav />
@@ -16,7 +37,12 @@ function App() {
          <Route path='/login'>
            <Login />
          </Route>
+         <Route path='/test'>
+           <Test/>
+        </Route> 
        </Switch>
+       {(!isLogin) ? <Redirect to={"/login"}/> : null}
+       {(isLogin) ? <Redirect to={"/test"}/> : null}
      </BrowserRouter>
     </div>
   );
--- client/src/components/Registration/Registration.js
+++ client/src/components/Registration/Registration.js
@@ -1,30 +1,45 @@
 import React from 'react';
-
+import { useRef } from 'react'
 function Registration(props) {
+
+const regHandler = async () => {
+  
+const uname = name.current.value;
+const upass = pass.current.value; 
+
+const response = await fetch('http://localhost:8080/users/register', {
+      method: 'POST',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        login: uname,
+        password: upass,
+      }),
+    });
+}
+
+const name = useRef();
+const pass = useRef();
 
   return (
     <div className="row">
     <form className="col s12">
+    <h4>Sign-up</h4>
       <div className="row">
         <div className="input-field col s12">
-          <input id="last_name" type="text" className="validate"/>
+          <input ref={name} id="last_name" type="text" className="validate"/>
           <label for="last_name">Name</label>
         </div>
       </div>
 
       <div className="row">
         <div className="input-field col s12">
-          <input id="password" type="password" className="validate"/>
+          <input ref={pass} id="password" type="password" className="validate"/>
           <label for="password">Password</label>
         </div>
       </div>
-      <div className="row">
-        <div className="input-field col s12">
-          <input id="email" type="email" className="validate"/>
-          <label for="email">Email</label>
-        </div>
-      </div>
-      <a class="waves-effect waves-light btn">button</a>
+     
+      <a onClick={regHandler} class="waves-effect waves-light btn">send</a>
     </form>
   </div>
   );
