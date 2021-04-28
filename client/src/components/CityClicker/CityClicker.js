357f4e291b98357835173079187e0236fabc7d56
Александр Чередниченко <chalexan@Aleksandrs-MacBook-Air.local>
2021-04-28 12:24:41 +0300

    dropdown selector

--- client/src/components/CityClicker/CityClicker.js
+++ client/src/components/CityClicker/CityClicker.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import M from 'materialize-css';
 
 function CityClicker(props) {
-const dispatch = useDispatch();
-const { currentCityLng, currentCityLat } = useSelector((state) => state);
-const moskowHandler = () => {
-  dispatch({ type: 'SETCITY', payload: {lat: 55.82115177407611, lng: 37.63916015625001 }})
-}
 
-const piterHandler = () => {
-  dispatch({ type: 'SETCITY', payload: {lat: 59.94352892477112, lng: 30.314025878906254 }})
-}
+  const dispatch = useDispatch();
+  const { currentCityLng, currentCityLat } = useSelector((state) => state);
+  const moskowHandler = () => {
+    dispatch({ type: 'SETCITY', payload: { lat: 55.82115177407611, lng: 37.63916015625001 } })
+  }
 
-const krasnodarHandler = () => {
-  dispatch({ type: 'SETCITY', payload: {lat: 45.0472082783978, lng:38.99047851562501 }})
-}
+  const piterHandler = () => {
+    dispatch({ type: 'SETCITY', payload: { lat: 59.94352892477112, lng: 30.314025878906254 } })
+  }
+
+  const krasnodarHandler = () => {
+    dispatch({ type: 'SETCITY', payload: { lat: 45.0472082783978, lng: 38.99047851562501 } })
+  }
+
+  useEffect(() => {
+    let elems = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(elems, { inDuration: 300, outDuration: 225, constrainWidth: false });
+  }, [])
 
   return (
 
     <div>
-       <a onClick={moskowHandler} class="waves-effect waves-light btn orange">Москва</a>
-       <a onClick={piterHandler} class="waves-effect waves-light btn orange">Санкт-Петербург</a>
-       <a onClick={krasnodarHandler} class="waves-effect waves-light btn orange">Краснодар</a>
+      <a class='dropdown-trigger btn w-20' href='#' data-target='dropdown1'>Выбери город</a>
+      <ul id='dropdown1' class='dropdown-content dropdown w-20'>
+        <li><a href="#!" constrainWidth onClick={moskowHandler}>Москва</a></li>
+        <li><a href="#!" constrainWidth onClick={piterHandler}>Санкт-Петербург</a></li>
+        <li><a href="#!" constrainWidth onClick={krasnodarHandler}>Краснодар</a></li>
+        <li><a href="#!" >Ростов</a></li>
+        <li><a href="#!" >Саратов</a></li>
+        <li><a href="#!" >Челябинск</a></li>
+        <li><a href="#!" >Ижевск</a></li>
+        <li><a href="#!" >Уфа</a></li>
+        <li><a href="#!" >Мурманск</a></li>
+      </ul>
     </div>
+
+
   );
 }
 
