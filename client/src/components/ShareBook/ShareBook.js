9f8301dc43684b8ad8a653b2bfeb3a28511cb6e7
Александр Чередниченко <chalexan@Aleksandrs-MacBook-Air.local>
2021-04-29 11:53:59 +0300

    add loc description to books

--- client/src/components/ShareBook/ShareBook.js
+++ client/src/components/ShareBook/ShareBook.js
@@ -1,96 +1,145 @@
-import React from 'react';
-import MapClickable from '../MapClickable/MapClickable';
-import CityClicker from '../CityClicker/CityClicker';
-import { useRef } from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import MapClickable from "../MapClickable/MapClickable";
+import CityClicker from "../CityClicker/CityClicker";
+import { useRef } from "react";
+import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 function ShareBook(props) {
-const history = useHistory();
+  const history = useHistory();
 
-const adress = useRef();
-const bookName = useRef();
-const bookDescription = useRef();
-const bookAuthor = useRef();
+  const adress = useRef();
+  const bookName = useRef();
+  const bookDescription = useRef();
+  const bookAuthor = useRef();
+  const placeDescription = useRef();
 
-const submitHandler = async() => {
-  const addBookResponse = await fetch('http://localhost:8080/book', {
-    method: 'POST',
-    credentials: 'include',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      creator: userName,
-      name: bookName.current.value,
-      author: bookAuthor.current.value,
-      info: bookDescription.current.value,
-      adress: adress.current.value,
-      latitude: currentLat,
-      longitude: currentLng,
-    }),
-  });
-  let book = await addBookResponse.json()
-  history.push(`/books/${book.id}`);
-}
-  const { currentLng, currentLat, userName, currentAdress } = useSelector((state) => state);
+  const submitHandler = async () => {
+    const addBookResponse = await fetch("http://localhost:8080/book", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        creator: userName,
+        name: bookName.current.value,
+        author: bookAuthor.current.value,
+        info: bookDescription.current.value,
+        description: placeDescription.current.value,
+        adress: adress.current.value,
+        latitude: currentLat,
+        longitude: currentLng,
+      }),
+    });
+    let book = await addBookResponse.json();
+    history.push(`/books/${book.id}`);
+  };
+  const { currentLng, currentLat, userName, currentAdress } = useSelector(
+    (state) => state
+  );
   return (
     <div className="center">
-        <div className="col s12 m7">
-          <div className="card">
-            <div className="card-content">
-              <h5>Поделись книгой!</h5>
-            </div>
-            <div className="">
-
-
+      <div className="col s12 m7">
+        <div className="card">
+          <div className="card-content">
+            <h5>Поделись книгой!</h5>
+          </div>
+          <div className="">
             <div class="row">
+              <div class="col s6">
+                <CityClicker />
+                <div className="input-field col s12">
+                  <input
+                    id="book-adress"
+                    type="text"
+                    className="validate"
+                    placeholder="Адрес книги"
+                    ref={adress}
+                    value={currentAdress}
+                  />
+                </div>
 
-      <div class="col s6">
-            <CityClicker/>
-      <div className="input-field col s12">
-          <input id="book-adress" type="text" className="validate" placeholder='Адрес книги' ref={adress} value={currentAdress}/>
-        </div>
-            
-        <div className="input-field col s12">
-          <input id="book-name" type="text" className="validate" placeholder='Название книги' ref={bookName}/>
-        </div>
-      
-        <div className="input-field col s12">
-          <input id="book-name" type="text" className="validate" placeholder='Автор книги' ref={bookAuthor}/>
-        </div>
-      
-        <div className="input-field col s12">
-          <input id="book-info" type="text" className="validate" placeholder='Описание книги' ref={bookDescription}/>
-        </div>
+                <div className="input-field col s12">
+                  <input
+                    id="book-adress"
+                    type="text"
+                    className="validate"
+                    placeholder="Описание места закладки"
+                    ref={placeDescription}
+                  />
+                </div>
 
-        <div className="input-field col s12">
-          <input id="book-info" type="text" className="validate" placeholder='Широта' value={currentLat}/>
-        </div>
+                <div className="input-field col s12">
+                  <input
+                    id="book-name"
+                    type="text"
+                    className="validate"
+                    placeholder="Название книги"
+                    ref={bookName}
+                  />
+                </div>
 
-        <div className="input-field col s12">
-          <input id="book-info" type="text" className="validate" placeholder='Долгота' value={currentLng}/>
-        </div>
+                <div className="input-field col s12">
+                  <input
+                    id="book-name"
+                    type="text"
+                    className="validate"
+                    placeholder="Автор книги"
+                    ref={bookAuthor}
+                  />
+                </div>
 
-      </div>
+                <div className="input-field col s12">
+                  <input
+                    id="book-info"
+                    type="text"
+                    className="validate"
+                    placeholder="Описание книги"
+                    ref={bookDescription}
+                  />
+                </div>
 
-      <div class="col s6">
-      <h6>Выбери место на карте</h6>
-      <br/>
-      <br/>
-      <div className="mar6p">
-              <MapClickable/>
-              <br/>
-              <button className="waves-effect waves-light btn orange" onClick = {submitHandler} style={{margin: '1rem'}}>Поделиться</button>
+                <div className="input-field col s12">
+                  <input
+                    id="book-info"
+                    type="text"
+                    className="validate"
+                    placeholder="Широта"
+                    value={currentLat}
+                  />
+                </div>
+
+                <div className="input-field col s12">
+                  <input
+                    id="book-info"
+                    type="text"
+                    className="validate"
+                    placeholder="Долгота"
+                    value={currentLng}
+                  />
+                </div>
+              </div>
+
+              <div class="col s6">
+                <div className="mar6p">Выбери место на карте</div>
+                <br />
+                <br />
+                <div className="mar6p">
+                  <MapClickable />
+                  <br />
+                  <button
+                    className="waves-effect waves-light btn orange"
+                    onClick={submitHandler}
+                    style={{ margin: "1rem" }}
+                  >
+                    Поделиться
+                  </button>
+                </div>
+              </div>
             </div>
-      </div>
-    </div>
-
-
-       
           </div>
         </div>
-        </div>
-        </div>
-      
+      </div>
+    </div>
   );
 }
 
--- client/src/components/Test/Test.js
+++ client/src/components/Test/Test.js
@@ -1,25 +1,24 @@
-import { Map, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { Map, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
-import './Test.css';
+import "./Test.css";
 
 const Test = () => {
   const [books, setBooks] = useState([]);
-  let [bookid,setBookid] = useState('');
+  let [bookid, setBookid] = useState("");
   const { currentCityLng, currentCityLat } = useSelector((state) => state);
   const position = [currentCityLat, currentCityLng];
   const dispatch = useDispatch();
   //tunk - redux
   const getBooks = async () => {
-    const response = await fetch('http://localhost:8080/books');
+    const response = await fetch("http://localhost:8080/books");
     const result = await response.json();
     setBooks(result.books);
     console.log(result.books);
     let markers = document.querySelectorAll(".leaflet-marker-icon");
     console.log(markers);
   };
- 
 
   useEffect(() => {
     getBooks();
@@ -32,16 +31,21 @@ const Test = () => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {books.map((el) => ( !el.isFound ?
-          <Marker position={[el.latitude, el.longitude]}>
-            <Popup>
-              Название : {el.name}. <br />
-              Адрес: {el.adress} <br />
-              Создатель: {el.creator} <br/>
-              <Link to='/take'>Взять книгy</Link>
-            </Popup>
-          </Marker> : null
-        ))}
+        {books.map((el) =>
+          !el.isFound ? (
+            <Marker position={[el.latitude, el.longitude]}>
+              <Popup>
+                Название : {el.name}. <br />
+                Адрес: {el.adress} <br />
+                Место закладки: {el.description} <br />
+                Первозакладчик: {el.creator} <br />
+                Latitude: {el.latitude} <br />
+                Longitude: {el.longitude} <br />
+                <Link to="/take">Взять книгy</Link>
+              </Popup>
+            </Marker>
+          ) : null
+        )}
       </Map>
     </div>
   );
