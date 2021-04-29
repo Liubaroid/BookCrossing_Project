e6a6f72d730e99cdfa95af57c771de022f6efd32
Александр Чередниченко <chalexan@Aleksandrs-MacBook-Air.local>
2021-04-29 08:44:42 +0300

    fix

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import Test from '../Test/Test'
-import CityClicker from '../CityClicker/CityClicker'
+import React from "react";
+import Test from "../Test/Test";
+import CityClicker from "../CityClicker/CityClicker";
 import { Link, useHistory } from "react-router-dom";
 
 function Main(props) {
-  const history = useHistory()
+  const history = useHistory();
   async function bookChecker(e) {
-    e.preventDefault()
-    let id = e.target.bookId.value.trim()
-   const response = await fetch(`http://localhost:8080/books/take/${id}`);
-      if (response.status === 500) {
-        alert('Книги с таким кодом нет');
-        return
-      }
-      history.push(`/books/${e.target.bookId.value}`);
+    e.preventDefault();
+    let id = e.target.bookId.value.trim();
+    const response = await fetch(`http://localhost:8080/books/take/${id}`);
+    if (response.status === 500) {
+      alert("Книги с таким кодом нет");
+      return;
+    }
+    history.push(`/books/${e.target.bookId.value}`);
   }
 
   return (
     <div className="">
       <div className="col s12 m7">
-        <h6 className="right-align" style={{ margin: '1rem' }} >Если вы любите ваши книги, позвольте им уйти.
-        (с) The New York Times
-      </h6>
+        <h6 className="right-align" style={{ margin: "1rem" }}>
+          Если вы любите ваши книги, позвольте им уйти. (с) The New York Times
+        </h6>
         <div className="card center">
           {/* <h4 className="#e3f2fd blue lighten-5" style={{padding: '1rem', height: '5rem', margin: '1rem' }}>Приветствие</h4> */}
           <div className="card-content container">
@@ -39,15 +39,36 @@ function Main(props) {
                 Следите как и с кем путешествует ваша книга!
             </h6>
             </div> */}
-            <div className="card-action" style={{ margin: '1rem' }}>
-              <Link to='/share'>Поделиться книгой</Link>
-              <Link to='/take'>Взять книгy</Link>
-              <Link to='/give'>Передать дальше</Link>
+            <div class="container">
+              <img
+                src="http://localhost:3000/logo.png"
+                alt="logo"
+                style={{ width: "100%" }}
+              />
             </div>
-            <form onSubmit={bookChecker} className="card-action container center">
-              <input type="text" name='bookId' className="center" placeholder="Ввести идентификатор книги" />
+
+            <div className="card-action" style={{ margin: "1rem" }}>
+              <Link to="/share">Поделиться книгой</Link>
+              <Link to="/take">Взять книгy</Link>
+              <Link to="/give">Передать дальше</Link>
+            </div>
+            <form
+              onSubmit={bookChecker}
+              className="card-action container center"
+            >
+              <input
+                type="text"
+                name="bookId"
+                className="center"
+                placeholder="Ввести идентификатор книги"
+              />
               <div>
-                <button className="waves-effect waves-light btn orange" style={{ margin: '1rem' }}>Найти книгy</button>
+                <button
+                  className="waves-effect waves-light btn orange"
+                  style={{ margin: "1rem" }}
+                >
+                  Найти книгy
+                </button>
               </div>
             </form>
           </div>
@@ -59,7 +80,6 @@ function Main(props) {
           <div className="center">
             <Test />
           </div>
-
         </div>
       </div>
     </div>
